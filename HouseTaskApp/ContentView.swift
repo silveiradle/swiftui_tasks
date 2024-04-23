5768e54fd8522f777a775ca04fcd736f707803d3
Leandro S Davila <leandrosdavila@gmail.com>
2024-04-23 10:07:23 -0300

    Initial Commit

---
+++ HouseTaskApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HouseTaskApp
+//
+//  Created by aluno06 on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HouseTaskApp/HouseTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseTaskAppApp.swift
+//  HouseTaskApp
+//
+//  Created by aluno06 on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HouseTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
